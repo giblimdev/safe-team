8b550a7113ee680fde9f454a72bb7d009edf62f1
jean-philippe Heurteux <158490322+giblimdev@users.noreply.github.com>
2025-02-18 17:59:31 +0100

    install prisma

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,143 +1,27 @@
-/*
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <a href="https://safe-team-igyvq35hp-giblimdevs-projects.vercel.app/" target="_blank">l'app on line</a>
+<h1>        retrouver les fonctionnalité de l'application</h1>
+
+<div>
+<h2>Liste des fonctionnalités :</h2>
+
+() ici les fonctionnalité et le bouton ajouter
 
 
-generator client {
-  provider = "prisma-client-js"
+<div>
+
+</div>
+
+
+</div>
+    </div>
+  )
 }
 
-datasource db {
-  provider = "postgresql"
-  url      = env("DATABASE_URL")
-}
+export default page
 
-model Idea {
-  id              String           @id @default(cuid())
-  functionalities Functionality[]
-}
-
-model Functionality {
-  id                String              @id @default(cuid())
-  devId             String
-  content           String
-  functionalityItems FunctionalityItem[]
-
-  Idea Idea[]
-}
-
-model FunctionalityItem {
-  id      String   @id @default(cuid())
-  item    String
-
-  Functionality Functionality[]
-}
-
-model User {
-  id          String      @id @default(cuid())
-  pseudo      String?     @db.VarChar(255)
-  email       String      @unique @db.VarChar(255)
-  password    String      @db.VarChar(255)
-  role        Role        @default(USER)
-  createdAt   DateTime    @default(now())
-  updatedAt   DateTime    @updatedAt
-  company     String
-  trainings   Training[]
-}
-
-enum Role {
-  USER
-  MANAGER
-  ADMIN
-}
-
-model Company {
-  id              String           @id @default(cuid())
-  name            String
-  employees       Employee[]
-  companyCertifs  CompanyCertif[]
-}
-
-model Employee {
-  id              String           @id @default(cuid())
-  name            String
-  email           String           @unique
-  companyId       String
-  company         Company          @relation(fields: [companyId], references: [id])
-  progress        Progress[]
-  employeeCertifs EmployeeCertif[]
-
-  CompanyCertif CompanyCertif[]
-}
-
-model Training {
-  id        String    @id @default(cuid())
-  title     String
-  modules   Module[]
-
-  User User[]
-}
-
-model Module {
-  id          String    @id @default(cuid())
-  title       String
-  chapters    Chapter[]
-  trainingId  String
-  training    Training  @relation(fields: [trainingId], references: [id])
-}
-
-model Chapter {
-  id        String    @id @default(cuid())
-  title     String
-  moduleId  String
-  module    Module    @relation(fields: [moduleId], references: [id])
-
-  Progress Progress[]
-}
-
-model Progress {
-  id          String    @id @default(cuid())
-  employeeId  String
-  employee    Employee  @relation(fields: [employeeId], references: [id])
-  chapterId   String
-  chapter     Chapter   @relation(fields: [chapterId], references: [id])
-  completed   Boolean   @default(false)
-}
-
-model EmployeeCertif {
-  id            String    @id @default(cuid())
-  issuedDate    DateTime  @default(now())
-  revisionDate  DateTime
-  softExpirDate DateTime
-  hardExpirDate DateTime
-  badgeImg      String
-  employeeId    String
-  employee      Employee  @relation(fields: [employeeId], references: [id])
-}
-
-model CompanyCertif {
-  id            String    @id @default(cuid())
-  issuedDate    DateTime  @default(now())
-  revisionDate  DateTime
-  softExpirDate DateTime
-  hardExpirDate DateTime
-  badgeImg      String
-  employeeId    String
-  employee      Employee  @relation(fields: [employeeId], references: [id])
-  companyId     String
-  company       Company   @relation(fields: [companyId], references: [id])
-}
-
-model Badge {
-  id              String    @id @default(cuid())
-  activBadge      String
-  warningBadge    String
-  deprecatedBadge String
-}
-
-
-
-
-
-
-
-
-*/
