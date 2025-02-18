094dffbfe4e5b394aefbc6381ef06f499d38922c
jean-philippe Heurteux <158490322+giblimdev@users.noreply.github.com>
2025-02-18 18:07:03 +0100

    deploye

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div><Link href="/dev">Dev</Link></div>
       </header>
       <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    
+    page
     </main></div>
   );
 }
